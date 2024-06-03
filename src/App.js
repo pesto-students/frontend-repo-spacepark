62e27ce1e65254f1b6fe13423f8661f07bae9796
Prachi-2002 <prachimadame01@gmail.com>
2024-06-04 00:09:29 +0530

    fix: added the login route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import SignUp from './components/SignUp.js';
+import Login from './components/Login/Login.js';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<SignUp />} />
-        {/* <Route path="/parking-space/:id" element={<ParkingSpace />} /> */}
+        <Route path="/login" element={< Login/>} />
         {/* <Route path="/booking" element={<Booking />} /> */}
         {/* <Route path="/user-profile" element={<UserProfile />} /> */}
       </Routes>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,18 @@
 // src/components/SignUp.js
 import React from 'react';
-import './Signup.scss';
-import { Link } from 'react-router-dom'; 
+import './Login.scss';
+import { Link } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-const SignupSchema = Yup.object().shape({
+const LoginSchema = Yup.object().shape({
   name: Yup.string().required('Name is required'),
   email: Yup.string().email('Invalid email').required('Email is required'),
   password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
   mobile: Yup.string().matches(/^[0-9]{10}$/, 'Mobile number must be 10 digits'),
 });
 
-const SignUp = () => {
+const Login = () => {
   return (
     <div>
       <div className='logo-design'>
@@ -30,7 +30,7 @@ const SignUp = () => {
             password: '',
             mobile: '',
           }}
-          validationSchema={SignupSchema}
+          validationSchema={LoginSchema}
           onSubmit={(values) => {
             console.log('Form data submitted:', values);
           }}
@@ -42,25 +42,17 @@ const SignUp = () => {
                 <ErrorMessage name="name" component="div" className="error"/>
               </div>
               <div>
-                <Field type="email" name="email" placeholder="Please enter your email"/>
-                <ErrorMessage name="email" component="div" className="error"/>
-              </div>
-              <div>
                 <Field type="password" name="password" placeholder="Please enter your password"/>
                 <ErrorMessage name="password" component="div" className="error"/>
-              </div>
-              <div>
-                <Field type="tel" name="mobile" placeholder="Please enter your mobile no (optional)"/>
-                <ErrorMessage name="mobile" component="div" className="error"/>
               </div>
               <button type="submit">Sign Up</button>
             </Form>
           )}
         </Formik>
       </div>
-      <p className='para-text'>Do you already have an account ? <Link to="/login"><span>Login here</span></Link></p>
+      <p>or <Link to="/signup" className='link'><span>Signup</span></Link></p>
     </div>
   );
 };
 
-export default SignUp;
+export default Login;
