0179e7c2a7ae674db9445984a5fbd78a081ea4f2
Anjani-22 <anjaniofficial22@gmail.com>
2024-06-24 18:11:17 +0530

    ticket styling

--- src/components/TicketsScreen/TicketCard.jsx
+++ src/components/TicketsScreen/TicketCard.jsx
@@ -1,6 +1,7 @@
 // TicketCard.js
 
 import React from "react";
+import "./TicketCard.scss";
 
 const TicketCard = ({ ticket }) => {
   const {
@@ -16,16 +17,25 @@ const TicketCard = ({ ticket }) => {
   } = ticket;
 
   return (
-    <div className="ticket-card">
-      <h3>Ticket ID: {id}</h3>
-      <p>User ID: {userId}</p>
-      <p>Parking Space ID: {parkingSpaceId}</p>
-      <p>Service ID: {serviceId}</p>
-      <p>Car Number: {carNumber}</p>
-      <p>Start Date: {startDate}</p>
-      <p>End Date: {endDate}</p>
-      <p>Price: {price}</p>
-      <p>Status: {status}</p>
+    <div className={`ticket-container ${status}`}>
+      <div className="ticket-content">
+        <h3 className="ticketId">Ticket ID: {id}</h3>
+        <p className={`status ${status}`}>Status: {status}</p>
+        <div className="timings">
+          <p>Start Date: {startDate}</p>
+          <p>End Date: {endDate}</p>
+        </div>
+        <div className="info user">
+          <p>User ID: {userId}</p>
+          <p>Car Number: {carNumber}</p>
+        </div>
+        <div className="info service">
+          <p>Parking Space ID: {parkingSpaceId}</p>
+          <p>Service ID: {serviceId}</p>
+        </div>
+
+        <p className="price">Price: &#8377;{price}</p>
+      </div>
     </div>
   );
 };
