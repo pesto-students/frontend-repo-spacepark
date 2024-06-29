c00a15a89c39a07140ace4b4f6e2fb085dd648ff
Anjani-22 <anjaniofficial22@gmail.com>
2024-06-29 11:26:32 +0530

    loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+// Loader.jsx
+import React from "react";
+import "./Loader.scss"; // Import the SCSS file
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loader__spinner"></div>
+    </div>
+  );
+};
+
+export default Loader;
